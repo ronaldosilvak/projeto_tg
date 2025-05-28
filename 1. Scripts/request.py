fc01de05e607be9ecefc5ae6e7debc1cd1e722ed
ronaldo <ronaldosilvanp.360@gmail.com>
2025-05-28 00:37:41 -0300

    adicionando database para input das informações

--- 1. Scripts/request.py
+++ 1. Scripts/request.py
@@ -4,26 +4,30 @@ import io
 import pandas as pd
 
 def baixar_e_processar_arquivo(url, file_type, dataframes):
-    response = requests.get(url)
-    
-    if response.status_code == 200:
-        if file_type == '.zip':
-            with zipfile.ZipFile(io.BytesIO(response.content)) as file:
-                # pegando o nome dos arquivos dentro do ZIP
-                arquivos = file.namelist()
-                for arquivo in arquivos:
-                    print(f'Processando o arquivo ZIP: {arquivo}')
+    try:
+        response = requests.get(url, stream=(file_type=='.csv'), timeout=60)
         
-
-        elif file_type == '.csv':
-            print(f'Processando o arquivo CSV: {url}')
-            df = pd.read_csv(io.StringIO(response.text), sep=';', encoding='utf-8-sig') 
-            print(df.head()) 
+        if response.status_code == 200:
+            if file_type == '.zip':
+                with zipfile.ZipFile(io.BytesIO(response.content)) as file:
+                    # pegando o nome dos arquivos dentro do ZIP
+                    arquivos = file.namelist()
+                    for arquivo in arquivos:
+                        print(f'Processando o arquivo ZIP: {arquivo}')
             
-            # Adicionando o DataFrame à lista
-            dataframes.append(df)
-    else:
-        print(f'Erro ao baixar o arquivo: {response.status_code}')
+
+            elif file_type == '.csv':
+                print(f'Processando o arquivo CSV: {url}')
+                df = pd.read_csv(io.StringIO(response.text), sep=';', encoding='utf-8-sig') 
+                print(df.head()) 
+                
+                # Adicionando o DataFrame à lista
+                dataframes.append(df)
+        else:
+            print(f'Erro ao baixar o arquivo: {response.status_code}')
+
+    except requests.exceptions.RequestException as e:
+        print(f'Erro ao tentar baixar o arquivo: {url}\n{e}')
 
 anos = range(2020, 2021)  
 semestres = ['01', '02']
@@ -47,5 +51,5 @@ for ano in anos:
 df_final = pd.concat(df_list, ignore_index=True)
 
 # Salvando o DataFrame final em um arquivo CSV
-df_final.to_csv('../dados_combinados.csv', index=False)
+df_final.to_csv('2. bases/dados_combinados.csv', index=False)
 print('Arquivo final salvo como dados_combinados.csv')
