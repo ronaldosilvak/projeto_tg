e4b4d902b2c60448a322df1e297341905cb677ec
ronaldo <ronaldosilvanp.360@gmail.com>
2025-05-27 23:55:45 -0300

    commit inicial

---
+++ 1. Scripts/request.py
@@ -0,0 +1,51 @@
+import requests
+import zipfile
+import io
+import pandas as pd
+
+def baixar_e_processar_arquivo(url, file_type, dataframes):
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        if file_type == '.zip':
+            with zipfile.ZipFile(io.BytesIO(response.content)) as file:
+                # pegando o nome dos arquivos dentro do ZIP
+                arquivos = file.namelist()
+                for arquivo in arquivos:
+                    print(f'Processando o arquivo ZIP: {arquivo}')
+        
+
+        elif file_type == '.csv':
+            print(f'Processando o arquivo CSV: {url}')
+            df = pd.read_csv(io.StringIO(response.text), sep=';', encoding='utf-8-sig') 
+            print(df.head()) 
+            
+            # Adicionando o DataFrame à lista
+            dataframes.append(df)
+    else:
+        print(f'Erro ao baixar o arquivo: {response.status_code}')
+
+anos = range(2020, 2021)  
+semestres = ['01', '02']
+
+
+df_list = []
+
+
+for ano in anos:
+    for semestre in semestres:
+        # Tentando primeiro o arquivo ZIP
+        url_zip = f'https://www.gov.br/anp/pt-br/centrais-de-conteudo/dados-abertos/arquivos/shpc/dsas/ca/ca-{ano}-{semestre}.zip'
+        baixar_e_processar_arquivo(url_zip, '.zip', df_list)  # Passa o tipo de arquivo como '.zip'
+
+        # Depois tenta o arquivo CSV
+        url_csv = f'https://www.gov.br/anp/pt-br/centrais-de-conteudo/dados-abertos/arquivos/shpc/dsas/ca/ca-{ano}-{semestre}.csv'
+        print(f'Baixando dados de: {url_csv}')
+        baixar_e_processar_arquivo(url_csv, '.csv', df_list)  # Passa o tipo de arquivo como '.csv'
+
+# Concatenando todos os DataFrames da lista em um único DataFrame
+df_final = pd.concat(df_list, ignore_index=True)
+
+# Salvando o DataFrame final em um arquivo CSV
+df_final.to_csv('../dados_combinados.csv', index=False)
+print('Arquivo final salvo como dados_combinados.csv')
